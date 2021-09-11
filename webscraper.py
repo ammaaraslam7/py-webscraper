30800d209aa958fcd692c53a292a4e87a0b74655
Ammaar Aslam <ammaaraslam7@gmail.com>
2021-09-11 17:35:04 +0530

    Script Updated

--- webscraper.py
+++ webscraper.py
@@ -1,1 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
 
+URL = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="firstHeading")
+print(results)
